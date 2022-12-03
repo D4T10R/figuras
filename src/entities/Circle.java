8937ab6a411e194ebb23078c4f69d1e193e55bae
Datior <leonardowederveiga@yahoo.com>
2022-12-03 12:22:38 -0300

    Implementação sem misturar interface com herança

---
+++ src/entities/Circle.java
@@ -0,0 +1,27 @@
+package entities;
+
+import model.Color;
+
+public class Circle extends Shape {
+    
+    private  Double radius;
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+}
